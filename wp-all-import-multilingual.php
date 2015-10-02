0d6fa731c3b21c26271fce9fb796142e1325fe77
Konrad Karpieszuk <konrad.k@icanlocalize.com>
2015-10-02 20:00:41 +0200

    Renamed plugin

--- wp-all-import-multilingual.php
+++ wp-all-import-multilingual.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-	Plugin Name: WP All Import - WPML Add-On
+	Plugin Name: WP All Import Multilingual
 	Plugin URI: http://wpml.org
 	Description: Import multilingual content to WordPress. Requires WP All Import & WPML.
 	Version: 2.0.0
@@ -53,9 +53,9 @@ if ( ! class_exists('WPAI_WPML') )
 		 * Return singletone instance
 		 * @return WPAI_WPML
 		 */
-		static public function getInstance() 
+		static public function getInstance()
 		{
-			if (self::$instance == NULL) 
+			if (self::$instance == NULL)
 			{
 				self::$instance = new self();
 			}
@@ -70,27 +70,27 @@ if ( ! class_exists('WPAI_WPML') )
 		private $language_code;
 
 		private function __construct()
-		{			
-			$this->wpml_addon = new RapidAddon('WPML Add-On', 'wpml_addon');						
+		{
+			$this->wpml_addon = new RapidAddon('WPML Add-On', 'wpml_addon');
 
 			$this->wpml_addon->set_import_function( array( &$this, 'import') );
 			$this->wpml_addon->set_post_saved_function( array( &$this, 'saved') );
 
 			$conditions = array(
 				'plugins' => array(
-					'sitepress-multilingual-cms/sitepress.php'						
+					'sitepress-multilingual-cms/sitepress.php'
 				)
 			);
 
-			$this->wpml_addon->admin_notice( 
+			$this->wpml_addon->admin_notice(
 				$this->wpml_addon->name . ' requires WP All Import <a href="http://www.wpallimport.com/" target="_blank">Pro</a> or <a href="http://wordpress.org/plugins/wp-all-import" target="_blank">Free</a> and <a href="https://wpml.org/" target="_blank">WPML Multilingual CMS.</a>',
 				$conditions
-			); 
+			);
 
-			$this->wpml_addon->run($conditions);														
+			$this->wpml_addon->run($conditions);
 
 			if ( $this->wpml_addon->is_active_addon('') )
-			{				
+			{
 				global $sitepress;
 
 				$this->wpml = $sitepress;
@@ -99,14 +99,14 @@ if ( ! class_exists('WPAI_WPML') )
 
 				$this->init_addon_fields();
 
-				add_action( 'admin_init', 			   array( &$this, 'enqueue_stylesheets' ) );	
+				add_action( 'admin_init', 			   array( &$this, 'enqueue_stylesheets' ) );
 				add_action( 'pmxi_before_post_import', array( &$this, 'before_post_import' ), 10, 1 );
 				add_action( 'pmxi_after_post_import',  array( &$this, 'after_post_import' ),  10, 1 );
 				add_action( 'pmxi_saved_post',         array( &$this, 'saved_post' ), 10, 1 );
 				add_action( 'pmxi_delete_post',        array( &$this, 'delete_post' ), 10, 1 );
 				add_filter( 'pmxi_import_name', 	   array( &$this, 'import_name'), 10, 2 );
-			}			
-			
+			}
+
 		}
 
 		/**
@@ -116,37 +116,37 @@ if ( ! class_exists('WPAI_WPML') )
 		*/
 		private function init_addon_fields()
 		{
-			global $wpdb;			
+			global $wpdb;
 
-			$table = PMXI_Plugin::getInstance()->getTablePrefix() . 'imports';			
+			$table = PMXI_Plugin::getInstance()->getTablePrefix() . 'imports';
 
 			$imports = $wpdb->get_results("SELECT * FROM $table WHERE `parent_import_id` = 0", ARRAY_A);
 
 			if ( empty($imports) )
 			{
-				$this->wpml_addon->add_text("This is your first import. Default language will be choosen automatically ( " . $this->get_flag_html($this->default_language) . $this->wpml->get_display_language_name($this->default_language, 'en') ." ).");				
+				$this->wpml_addon->add_text("This is your first import. Default language will be choosen automatically ( " . $this->get_flag_html($this->default_language) . $this->wpml->get_display_language_name($this->default_language, 'en') ." ).");
 			}
 
-			$langs = $this->wpml->get_active_languages();	
+			$langs = $this->wpml->get_active_languages();
 
 			if ( ! empty($langs) )
 			{
 				// prepare active languages list
 				$language_list = array();
 
-				foreach ($langs as $code => $langInfo) 
+				foreach ($langs as $code => $langInfo)
 				{
 					$language_list[$code] = $this->get_flag_html($code) . $langInfo['display_name'];
 
 					if ($code == $this->default_language) $language_list[$code] .= ' ( <strong>default</strong> )';
-				}	
+				}
 
 				$this->wpml_addon->add_field(
 					'lng',
 					'Content Language',
 					'radio',
 					$language_list
-				);				
+				);
 
 				if ( ! empty($imports) )
 				{
@@ -156,8 +156,8 @@ if ( ! class_exists('WPAI_WPML') )
 					foreach ( $imports as $import )
 					{
 						if ( ! empty($_GET['id']) and $_GET['id'] == $import['id'] ) continue;
-						$import_options = unserialize($import['options']);						
-						$import_list[$import['id']]  = '[ ID: ' . $import['id'] . ' ] ' . ( ( ! empty($import_options['wpml_addon']['lng'])) ? $this->get_flag_html($import_options['wpml_addon']['lng']) : '' ) . (( ! empty($import['friendly_name']) ) ? $import['friendly_name'] : $import['name']); 						
+						$import_options = unserialize($import['options']);
+						$import_list[$import['id']]  = '[ ID: ' . $import['id'] . ' ] ' . ( ( ! empty($import_options['wpml_addon']['lng'])) ? $this->get_flag_html($import_options['wpml_addon']['lng']) : '' ) . (( ! empty($import['friendly_name']) ) ? $import['friendly_name'] : $import['name']);
 					}
 
 					$this->wpml_addon->add_options(
@@ -181,7 +181,7 @@ if ( ! class_exists('WPAI_WPML') )
 										$this->wpml_addon->add_field('unique_key', 'Unique Key', 'text', null, 'To inform WPML that this new post is translation of another post put the same unique key like you did for post in main language.')
 									)
 								)
-							)							
+							)
 						)
 					);
 
@@ -190,13 +190,13 @@ if ( ! class_exists('WPAI_WPML') )
 					// $this->wpml_addon->add_options(
 					// 	null,
 					// 	'Advanced Settings',
-					// 	array(							
+					// 	array(
 					// 		$this->wpml_addon->add_field('translate_taxes', 'Translate taxonomies', 'radio', array('' => 'No', '1' => 'Yes'))
-					// 	) 
+					// 	)
 					// );
 				}
 				return;
-			}			
+			}
 
 			$this->wpml_addon->add_text('Please set up site languages before using \'WP All Import - WPML add-on.\'');
 		}
@@ -232,12 +232,12 @@ if ( ! class_exists('WPAI_WPML') )
 		// [actions]
 
 			public function enqueue_stylesheets()
-			{		
-				wp_enqueue_style( 'wp-all-import-wpml-add-on', self::ROOT_URL . '/static/css/admin.css', false, 0.1, 'all' ); 
-			}		
-			
+			{
+				wp_enqueue_style( 'wp-all-import-wpml-add-on', self::ROOT_URL . '/static/css/admin.css', false, 0.1, 'all' );
+			}
+
 			/**
-			*				
+			*
 			*	Fires before inserting/updating post [do_action - 'pmxi_before_post_import']
 			*
 			*/
@@ -246,7 +246,7 @@ if ( ! class_exists('WPAI_WPML') )
 				$this->current_language = apply_filters('wpml_current_language', null);
 
 				if ( empty($this->language_code) )
-				{				
+				{
 					$import = new PMXI_Import_Record();
 					$import->getById( $import_id );
 					if ( ! $import->isEmpty() )
@@ -303,11 +303,11 @@ if ( ! class_exists('WPAI_WPML') )
 		*
 		*/
 		public function import($post_id, $data, $import, $articleData, $logger)
-		{			
+		{
 			// return if this is a basic post ( not a translation )
 			if ( empty($import['options']['wpml_addon']['matching_logic']) or empty($import['options']['wpml_addon']['import']) ) return;
 
-			// search for post to translate			
+			// search for post to translate
 			$parentImport = new PMXI_Import_Record();
 			$parentImport->getById($import['options']['wpml_addon']['import']);
 
@@ -316,47 +316,47 @@ if ( ! class_exists('WPAI_WPML') )
 				// post is that must be translated
 				$parent_post_id = false;
 
-				$postRecord = new PMXI_Post_Record();	
+				$postRecord = new PMXI_Post_Record();
 				$postRecord->clear();
 				$postRecord->getBy(array(
 					'unique_key' => $data['unique_key'],
 					'import_id'  => $import['options']['wpml_addon']['import']
 				));
-				if ( ! $postRecord->isEmpty() ) 
+				if ( ! $postRecord->isEmpty() )
 					$parent_post_id = $postRecord->post_id;
 
 				if ($parent_post_id)
-				{	
+				{
 					$post_type = (in_array(get_post_type($post_id), array('product', 'product_variation'))) ? 'post_product' : 'post_' . get_post_type($post_id);
 
-					$trid = $this->wpml->get_element_trid($parent_post_id, $post_type);					
-					
+					$trid = $this->wpml->get_element_trid($parent_post_id, $post_type);
+
 					if ( $trid )
 					{
 						global $wpdb;
 
 						// sync translation slug
-						$parent_post = get_post($parent_post_id);									
+						$parent_post = get_post($parent_post_id);
 
-						if ( ! empty($parent_post) and $parent_post->post_title == $articleData['post_title']) 
-						{			
-							$wpdb->update( $wpdb->posts, array( 'post_name' => $parent_post->post_name ), array( 'ID' => $post_id ) );				
+						if ( ! empty($parent_post) and $parent_post->post_title == $articleData['post_title'])
+						{
+							$wpdb->update( $wpdb->posts, array( 'post_name' => $parent_post->post_name ), array( 'ID' => $post_id ) );
 						}
 
 						// create a translation
 						$tid = $this->wpml->set_element_language_details($post_id, $post_type, $trid, $import->options['wpml_addon']['lng'], $parentImport->options['wpml_addon']['lng']);
 
-						if (is_wp_error($tid)) 
+						if (is_wp_error($tid))
 						{
 							$logger and call_user_func($logger, __('<b>ERROR</b>', 'wp_all_import_plugin') . ': ' . $tid->get_error_message());
 						}
 						else
 						{
-							$logger and call_user_func($logger, sprintf(__('- Created `%s` translation for `%s`', 'wp_all_import_plugin'), $import->options['wpml_addon']['lng'], $parent_post->post_title));	
-						}												
+							$logger and call_user_func($logger, sprintf(__('- Created `%s` translation for `%s`', 'wp_all_import_plugin'), $import->options['wpml_addon']['lng'], $parent_post->post_title));
+						}
 					}
 				}
-			}						
+			}
 		}
 
 		/**
